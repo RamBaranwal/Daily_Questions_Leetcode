8c212ca17533d3361c32160e8569361c19888d78
ram_baranwal <rambaranwalup50@gmail.com>
2025-12-11 23:55:12 +0530

    updated 2 data type

--- DataType.java
+++ DataType.java
@@ -1,26 +1,26 @@
 /*
-Java has 8 primitive data types; *char, boolean, byte, short, int, long, float, and double*. For this exercise, we'll work with the primitives used to hold integer values (*byte, short, int,* and *long*):
+Java has 8 primitive data types;*char, boolean, byte, short, int, long, float, and double*. For this exercise, we'll work with the primitives used to hold integer values (*byte, short, int,* and *long*):
 
-- A *byte* is an 8-bit signed integer.
-- A *short* is a 16-bit signed integer.
-- An *int* is a 32-bit signed integer.
-- A *long* is a 64-bit signed integer.
+- A*byte*]is an 8-bit signed integer.
+- A*short*is a 16-bit signed integer.
+- A*int*is a 32-bit signed integer.
+- A*long*is a 64-bit signed integer.
 
 Given an input integer, you must determine which primitive data types are capable of properly storing that input.
 
 To get you started, a portion of the solution is provided for you in the editor.
 
-**Reference:** https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+**Reference:**https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
 
 **Input Format**
 
-The first line contains an integer, T, denoting the number of test cases.
+The first line contains an integer,T, denoting the number of test cases.
 
-Each test case, T, is comprised of a single line with an integer, n, which can be arbitrarily large or small.
+Each test case,T, is comprised of a single line with an integer,n, which can be arbitrarily large or small.
 
 **Output Format**
 
-For each input variable n and appropriate primitive dataType, you must determine if the given primitives are capable of storing it. If yes, then print:
+For each input variabl and appropriate primitive dataType, you must determine if the given primitives are capable of storing it. If yes, then print:
 
 ```
 n can be fitted in:
@@ -69,7 +69,7 @@ n can't be fitted anywhere.
 2133333333333333333333333333333333333333 is very large and is outside of the allowable range of values for the primitive data types discussed in this problem.
 */
 
-import java.io.*;
+// import java.io.*;
 import java.util.*;
 
 public class DataType {
