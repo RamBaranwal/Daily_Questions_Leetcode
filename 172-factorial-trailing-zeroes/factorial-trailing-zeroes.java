18e25ee189f8be760e494625bf0a52532a23022e
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-04-04 22:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (43.02%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,5 +1,8 @@
 class Solution {
     public int trailingZeroes(int n) {
+        // formula
+        // n / 5 and count the quetionet till n != 0
+        // this is the trick to know the trialing zero 
         int count = 0;
         while( n > 0){
             n = n / 5;
