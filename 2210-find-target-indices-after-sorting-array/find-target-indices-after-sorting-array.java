50ec9bfece4b48d8894e02ffb52693121c58f9c1
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-04-11 18:57:23 +0530

    Time: 6 ms (27.95%) | Memory: 48.6 MB (22.36%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
-        List<Integer> list = new ArrayList<>();
-        helper(nums, target, 0, list);
-        return list;
+        return helper(nums, target, 0);
     }
-    private List<Integer> helper(int nums[], int target, int index, List<Integer> list){
+    private List<Integer> helper(int[] nums, int target, int index){
+        List<Integer> res = new ArrayList<>();
         if(nums.length == index){
-            return list;
+            return res;
         }
         if(nums[index] == target){
-            list.add(index);
+            res.add(index);
         }
-        return helper(nums, target, index + 1, list);
+        List<Integer> addBelowCalls = helper(nums, target, index + 1);
+        res.addAll(addBelowCalls);
+        return res;
     }
 }
