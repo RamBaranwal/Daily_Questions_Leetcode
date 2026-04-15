2d905daaafa42c5987a7460dbe4848814092c46b
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-04-15 15:30:18 +0530

    Time: 1 ms (29.87%) | Memory: 47 MB (42.86%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,35 +1,13 @@
 class Solution {
-    public int closestTarget(String[] arr, String target, int startIndex) {
-        // see backward direction 
-        int n = arr.length;
-        int k = startIndex;
-        int backCount = 0;
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
         for(int i = 0; i < n; i++){
-            if(k < 0){
-                k = n - 1;
-            }
-            if(!arr[k--].equals(target)){
-                backCount++;
-            }
-            else{
-                break;
+            int forward = (startIndex + i) % n;
+            int backward = (startIndex - i + n) % n;
+            if(words[forward].equals(target) || words[backward].equals(target)){
+                return i;
             }
         }
-
-        // see forward direction
-        int frontCount = 0;
-        int l = startIndex;
-        for(int i = 0; i < n; i++){
-            if(l > n - 1){
-                l = 0;
-            }
-            if(!arr[l++].equals(target)){
-                frontCount++;
-            }
-            else{
-                break;
-            }
-        }
-        return Math.min(frontCount, backCount) == n ? -1 : Math.min(frontCount, backCount);
+        return -1;
     }
 }
