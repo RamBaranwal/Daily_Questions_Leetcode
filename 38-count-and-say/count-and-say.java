0a7efecf8532dee7eefc5d2916c7b7aea5b40afb
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-05-08 23:40:13 +0530

    Time: 23 ms (5.52%) | Memory: 53 MB (5.14%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,26 +1,29 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n == 1) return "1";
-        String s = countAndSay(n - 1);
+        if(n == 1){
+            return "1";
+        }
+        
+        String prev = countAndSay(n - 1);
 
-        String ans = "";
+        String res = "";
 
-        int i = 0; 
-        int j = 0;
-        while(j < s.length()){
-            if(s.charAt(i) == s.charAt(j)){
-                j++;
-            }
-            else{
-                int dif = j - i;
-                ans = ans + dif;
-                ans = ans + s.charAt(i);
+        int i = 0;
+        int lastI = 0;
+        int lastJ = 0;
+        for(int j = 0; j < prev.length(); j++){
+            if(prev.charAt(i) != prev.charAt(j)){
+                int diff = j - i;
+                res = res + diff;
+                res = res + prev.charAt(i);
                 i = j;
             }
+            lastI = i;
+            lastJ = j;
         }
-        int dif = j - i;
-        ans = ans + dif;
-        ans = ans + s.charAt(i);
-        return ans;
+        int diff = lastJ - lastI + 1;
+        res = res + diff;
+        res = res + prev.charAt(lastI);
+        return res;
     }
 }
