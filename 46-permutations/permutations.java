41cd32cb301a753b7a80baa223cf3a4397c41c96
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-05-18 23:28:40 +0530

    Time: 1 ms (86.88%) | Memory: 45.6 MB (45.95%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        helper(res, new ArrayList<>(), nums, 0, new boolean[nums.length]);
         return res;
     }
-
-    private void helper(List<List<Integer>> res, List<Integer> current, int[] nums, boolean[] used){
-        if(nums.length == current.size()){
+    private void helper(List<List<Integer>> res, List<Integer> current,int[] nums, int index, boolean[] used){
+        if(nums.length == index){
             res.add(new ArrayList<>(current));
             return;
         }
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0 ; i < nums.length; i++){
             if(used[i]){
                 continue;
             }
 
             current.add(nums[i]);
             used[i] = true;
-            helper(res, current, nums, used);
+            helper(res, current, nums, index + 1, used);
 
             current.remove(current.size() - 1);
             used[i] = false;
