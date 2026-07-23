fb4c13d3b9d764610ba0ce537caf42b6a34f06ae
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-07-23 17:10:21 +0530

    Time: 118 ms (27.00%) | Memory: 48.2 MB (7.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,24 +1,37 @@
 class Solution {
-    int maxLen = 0;
-    int start = 0;
     public String longestPalindrome(String s) {
-        for(int i = 0; i < s.length(); i++){
-            expand(s, i, i);
-            expand(s, i, i + 1);
+        // into dp
+        int n = s.length();
+        int start = 0;
+        int maxLen = 0;
+        boolean[][] dp = new boolean[n][n];
+        // making single one to be palidromic because a is palindromic to a
+        for(int i = 0; i < n; i++){
+            maxLen = 1;
+            dp[i][i] = true;
         }
+
+        // making 2 common palindrmic 
+        for(int i = 0; i < n - 1; i++){
+            if(s.charAt(i) == s.charAt(i + 1)){
+                dp[i][i + 1] = true;
+                start = i;
+                maxLen = 2;
+            }
+        }
+
+        // rest of the values
+        for(int len = 3; len <= n; len++){
+            for(int i = 0; i <= n - len; i++){
+                int j = i + len - 1;
+                if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]){
+                    dp[i][j] = true;
+                    start = i;
+                    maxLen = len;
+                }
+            }
+        }
+
         return s.substring(start, start + maxLen);
-    }
-
-    private void expand(String s, int left, int right){
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left--;
-            right++;
-        }
-
-        int len = right - left - 1;
-        if(len > maxLen){
-            maxLen = len;
-            start = left + 1;
-        }
     }
 }
