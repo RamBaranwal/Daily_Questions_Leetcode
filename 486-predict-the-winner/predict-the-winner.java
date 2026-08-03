24908fcbd1e67c577c5a555c814cb91c9cc4dd2d
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-08-03 21:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (32.16%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        Integer[][] dp = new Integer[nums.length][nums.length];
-
-        return solve(nums, dp, 0, nums.length - 1) >= 0;
-    }
-    private int solve(int[] nums, Integer[][] dp, int left, int right){
-        if(left == right){
-            return nums[left];
+        int[][] dp = new int[nums.length][nums.length];
+        for(int i = 0; i < nums.length; i++){
+            dp[i][i] = nums[i];
         }
 
-        if(dp[left][right] != null){
-            return dp[left][right];
-        }
-        
-        int leftTaken = nums[left] - solve(nums, dp, left + 1, right);
-        int rightTaken = nums[right] - solve(nums, dp, left, right - 1);
+        for(int len = 2; len <= nums.length; len++){
+            for(int left = 0; left <= nums.length - len; left++){
+                int right = left + len - 1;
 
-        return dp[left][right] = Math.max(leftTaken, rightTaken);
+                int takingLeft = nums[left] - dp[left + 1][right];
+                int takingRight = nums[right] - dp[left][right - 1];
+
+                dp[left][right] = Math.max(takingLeft, takingRight);
+            }
+        }
+        return dp[0][nums.length - 1] >= 0;
     }
 }
