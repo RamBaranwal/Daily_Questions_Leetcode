1fbb261b214318a1bcafc4b53ff5205acc804ce7
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-08-31 11:54:07 +0530

    Time: 2 ms (46.53%) | Memory: 42 MB (84.15%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -1,30 +1,13 @@
 class Solution {
     public int minimumSum(int num) {
-        int digit = 0;
-        int[] fre = new int[10];
+        int[] nums = new int[4];
+        int i = 0;
         while(num != 0){
-            int rem = num % 10;
-            fre[rem]++;
+            nums[i++] = num % 10;
             num /= 10;
         }
-        // System.out.println(Arrays.toString(fre));
-        for(int i = 0; i < 10; i++){
-            while(fre[i] > 0){
-                digit = digit * 10 + i;
-                fre[i]--;
-            }
-        }
-        // System.out.println(digit);
-        String str = Integer.toString(digit);
-        while(str.length() != 4){
-            str = "0" + str;
-        }
-        // System.out.println(str);
-        String str1 = "" + str.charAt(0) + str.charAt(2);
-        String str2 = "" + str.charAt(1) + str.charAt(3);
-        int new1 = Integer.parseInt(str1);
-        int new2 = Integer.parseInt(str2);
-        return new1 + new2;
-        // return 0;
+
+        Arrays.sort(nums);
+        return (nums[0] * 10 + nums[2]) + (nums[1] * 10 + nums[3]);
     }
 }
