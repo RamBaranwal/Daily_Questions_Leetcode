ca77c87277d903c7280730fdbd24e6d5cbc169ce
ram_baranwal <143525627+RamBaranwal@users.noreply.github.com>
2026-09-12 20:04:00 +0530

    Time: 94 ms (5.08%) | Memory: 47.5 MB (7.56%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -1,45 +1,34 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        boolean[] used = new boolean[digits.length];
-        boolean[] number = new boolean[1000];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int digit : digits){
+            map.put(digit, map.getOrDefault(digit, 0) + 1);
+        }
+
         int count = 0;
+        for(int num = 100; num <= 998; num += 2){
+            int a = (num / 100);
+            int b = (num / 10) % 10;
+            int c = (num % 10);
 
-        for(int i = 0; i < digits.length; i++){
-            if(digits[i] == 0){
-                continue;
-            }
-            used[i] = true;
+            HashMap<Integer, Integer> required = new HashMap<>();
+            required.put(a, required.getOrDefault(a, 0) + 1);
+            required.put(b, required.getOrDefault(b, 0) + 1);
+            required.put(c, required.getOrDefault(c, 0) + 1);
 
-            for(int j = 0; j < digits.length; j++){
-                if(used[j]){
-                    continue;
+            boolean possible = true;
+
+            for(int digit : required.keySet()){
+                if(required.get(digit) > map.getOrDefault(digit, 0)){
+                    possible = false;
+                    break;
                 }
-                used[j] = true;
-                
-                for(int k = 0; k < digits.length; k++){
-                    if(used[k]){
-                        continue;
-                    }
-
-                    if(digits[k] % 2 != 0){
-                        continue;
-                    }
-
-                    int num = digits[i] * 100 +
-                            digits[j] * 10 +
-                            digits[k];
-                    // System.out.println(num);
-                    if(!number[num]){
-                        number[num] = true;
-                        count++;
-                        // System.out.println(count);
-                    }
-                }
-
-                used[j] = false;
             }
 
-            used[i] = false;
+            if(possible){
+                count++;
+            }
         }
         return count;
     }
